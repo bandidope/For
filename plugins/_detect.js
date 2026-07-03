9cb2ef0f88edb7737234d58a980961a09f56fa77
YALLICO OWNER <yallico2024@gmail.com>
2026-07-03 13:24:33 -0500

    Actualizar _detect.js

--- plugins/_detect.js
+++ plugins/_detect.js
@@ -1,38 +1,33 @@
-export async function before(m, { conn }) {
-    if (!m.messageStubType ||!m.isGroup) return true
+let handler = m => m
+handler.all = async function (m) {
+    if (!m.messageStubType ||!m.isGroup) return 
+    
+    let who = m.messageStubParameters[0] + '@s.whatsapp.net'
+    let actor = m.key.participant || m.participant 
+    if (!actor) return // Si no hay actor, se sale. Pa evitar crash
 
-    let who = m.messageStubParameters[0] + '@s.whatsapp.net' // La víctima / El afectado
-    let actor = m.key.participant || m.participant // El admin que hizo la acción
     let chat = m.chat
     let user = who.split('@')[0]
     let admin = actor.split('@')[0]
 
     try {
         switch (m.messageStubType) {
-
-        // 25 = PROMOTE = Le dieron admin a alguien
-        case 25: 
-            await conn.reply(chat, `👑 @${admin} le dio *ADMIN* a @${user}\nNo abuses del poder oe 😂`, m, { mentions: [actor, who] })
-            break
-
-        // 26 = DEMOTE = Le quitaron admin a alguien
-        case 26: 
-            await conn.reply(chat, `💀 @${admin} le quitó *ADMIN* a @${user}\nF, bajaste de rango`, m, { mentions: [actor, who] })
-            break
-
-        // 21 = SUBJECT = Cambiaron el nombre del grupo
-        case 21: 
-            let tema = m.messageStubParameters[0]
-            await conn.reply(chat, `📢 @${admin} cambió el nombre del grupo\nNuevo nombre: *${tema}*`, m, { mentions: [actor] })
-            break
-
-        // 22 = ICON = Cambiaron la foto del grupo
-        case 22: 
-            await conn.reply(chat, `🖼️ @${admin} cambió la foto del grupo\nQuedó god el nuevo icono`, m, { mentions: [actor] })
-            break
+            case 25: // PROMOTE
+                await conn.reply(chat, `👑 @${admin} le dio *ADMIN* a @${user}`, m, { mentions: [actor, who] })
+                break
+            case 26: // DEMOTE 
+                await conn.reply(chat, `💀 @${admin} le quitó *ADMIN* a @${user}`, m, { mentions: [actor, who] })
+                break
+            case 21: // SUBJECT
+                let tema = m.messageStubParameters[0]
+                await conn.reply(chat, `📢 @${admin} cambió el nombre a: *${tema}*`, m, { mentions: [actor] })
+                break
+            case 22: // ICON
+                await conn.reply(chat, `🖼️ @${admin} cambió la foto del grupo`, m, { mentions: [actor] })
+                break
         }
-
     } catch (e) {
-        console.log('Error en _detect:', e)
+        console.log(e)
     }
 }
+export default handler
