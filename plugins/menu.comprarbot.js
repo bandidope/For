524c2f980946f7fafdd76e2b1245c664a5378d0f
YALLICO OWNER <yallico2024@gmail.com>
2026-07-04 05:59:56 -0500

    Actualizar menu.comprarbot.js

--- plugins/menu.comprarbot.js
+++ plugins/menu.comprarbot.js
@@ -2,13 +2,14 @@ let handler = async (m, { conn }) => {
   let txt = `*🛒 [ VENTA DE BOTS FOR THREE ]* 🛒\n\n`
   txt += `━━━━━━━━━━\n`
   txt += `🤖 *Bot For Three MD* Full Plugins\n`
-  txt += `💰 *Precio:* S/15 soles o $4 USD\n`
-  txt += `📦 *Incluye:* +40 Comandos, Menú, Autoresponder\n`
-  txt += `⚡ *Instalación:* 10 min vía Termux/Pc\n`
-  txt += `🛡️ *Soporte:* 7 días gratis\n`
+  txt += `💰 *Precio:* S/30 soles o $11 USD\n`
+  txt += `📦 *Incluye:* +10p Comandos, Menú, Autoresponder Funcionales Sin Errores\n`
+  txt += `⚡ *Instalación:* 10 min vía Hosting\n`
+  txt += `🛡️ *Soporte:* 24/7\n`
   txt += `━━━━━━━━━━\n\n`
   txt += `👤 *Vendedor:* Yallico\n`
   txt += `📱 *WhatsApp:* wa.me/51936994155\n`
+txt += `🌀 *Link:* \n`
   txt += `👥 *Grupo:* https://chat.whatsapp.com/LjPhgjqCM934QEzYz3vrVk\n\n`
   txt += `> *Info:*.catalogo | *Garantía:*.garantia`
   
