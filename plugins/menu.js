aa963def91993bb544877780a623feb538c9d6e8
YALLICO OWNER <yallico2024@gmail.com>
2026-07-07 10:29:37 -0500

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,38 +2,30 @@ let handler = async (m, { conn }) => {
   const imgUrl = 'https://files.evogb.win/7Rs2Rz.jpg'
   let taguser = '@' + m.sender.split('@')[0]
 
-  let plugins = Object.values(global.plugins).filter(p =>!p.disabled && p.help)
-  let totalCmds = plugins.reduce((acc, p) => acc + [].concat(p.help).length, 0)
-
   let txt = `
 *🤖 [ FOR THREE BOT v3.2 ]* 🤖
 
 👤 Usuario: ${taguser}
-📦 Total Comandos: ${totalCmds}
 ⚙️ Prefijo: [. ]
 
-*Elige una categoría* 👇
+Toca el botón para ver todas las categorías 👇
 `.trim()
 
   let buttons = [
-    { buttonId: '.punto menu fun', buttonText: { displayText: '🎮 JUEGOS FUN' }, type: 1 },
-    { buttonId: '.punto menu info', buttonText: { displayText: 'ℹ️ INFO' }, type: 1 },
-    { buttonId: '.punto menu main', buttonText: { displayText: '⚙️ MAIN' }, type: 1 },
-    { buttonId: '.punto menu sorteos', buttonText: { displayText: '🎁 SORTEOS' }, type: 1 },
-    { buttonId: '.punto menu ventas bot', buttonText: { displayText: '🛒 VENTAS' }, type: 1 }
+    { buttonId: '.menu categorias', buttonText: { displayText: '📋 VER COMANDOS' }, type: 1 }
   ]
 
   await conn.sendMessage(m.chat, {
     image: { url: imgUrl },
     caption: txt,
-    footer: 'Toca el botón de la categoría',
+    footer: 'FOR THREE BOT',
     buttons: buttons,
     headerType: 4,
     mentions: [m.sender]
   }, { quoted: m })
 }
 
-handler.command = /^(punto\s?menu|pm)$/i //.punto menu o.pm
+handler.command = /^(menu|menú|help)$/i //.menu
 handler.tags = ['main']
 handler.help = ['menu']
 export default handler
